7a25c18eaaf27a96f6fba9255194e44cec30dbb4
Jeffrey S. Vetter <vetter@computer.org>
2026-04-21 12:52:54 -0400

    touch up the script

--- vetter-update-all.sh
+++ vetter-update-all.sh
@@ -4,6 +4,5 @@ set -e
 uv run --with pandas --with requests --with pyyaml  vetter-update-pres.py
 uv run --with pandas --with requests --with pyyaml  vetter-update-service.py
 uv run --with pandas --with requests --with pyyaml  vetter-update-software.py
-
 uv run --with pandas --with requests --with pyyaml --with python-frontmatter --with geopy --with getorg vetter-update-talkmap.py
 
